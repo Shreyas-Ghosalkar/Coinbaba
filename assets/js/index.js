b54e1a7932beda241e059097bd0e3e225ee37f69
Shreyas Ghosalkar <shreyasghosalkar@gmail.com>
2021-10-14 23:56:26 +0530

    make it better new8

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,7 +17,7 @@ $("#update_user").submit(function(event){
 
     var id = $(this).attr("data-id")
     var request = {
-        "url" : `http://localhost:3000/api/users/${data.id}`,
+        "url" : `https://coinbaba.herokuapp.com/api/users/${data.id}`,
         "method" : "PUT",
 
     }
@@ -35,7 +35,7 @@ if(window.location.pathname == "/"){
         var id = $(this).attr("data-id")
 
         var request = {
-            "url" : `http://localhost:3000/api/users/${id}`,
+            "url" : `https://coinbaba.herokuapp.com/api/users/${id}`,
             "method" : "PUT"
         }
 
