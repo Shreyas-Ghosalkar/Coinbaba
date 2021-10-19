580f5872f542aa089797f41d441861d8262256ab
Shreyas-Ghosalkar <92569990+Shreyas-Ghosalkar@users.noreply.github.com>
2021-10-19 12:48:23 +0530

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,7 +17,7 @@ $("#update_user").submit(function(event){
 
     var id = $(this).attr("data-id")
     var request = {
-        "url" : `http://coinbaba.herokuapp.com/api/users/${data.id}`,
+        "url" : `https://coinbaba.herokuapp.com/api/users/${data.id}`,
         "method" : "PUT",
 
     }
@@ -36,7 +36,7 @@ if(window.location.pathname == "/"){
         var hasvoted=Cookies.get("Voted")
         if(hasvoted !="true"){
             var request = {
-                "url" : `http://coinbaba.herokuapp.com/api/users/${id}`,
+                "url" : `https://coinbaba.herokuapp.com/api/users/${id}`,
                 "method" : "PUT"
             }
 
