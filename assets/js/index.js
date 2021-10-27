3c063d79a9048784f2abff7b29877c0bf20b19d2
rohantirmale <34538719+rohantirmale@users.noreply.github.com>
2021-10-27 13:56:58 +0530

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -28,27 +28,47 @@ $("#update_user").submit(function(event){
 
 })
 
+function cookiesEnabled() 
+    {
+        var cookiesEnabled = (navigator.cookieEnabled) ? true : false;
+
+        if (typeof navigator.cookieEnabled == "undefined" && !cookiesEnabled) 
+        {
+            document.cookie = "mytestcookie";
+            cookiesEnabled = (document.cookie.indexOf("mytestcookie") != -1) ? true : false;
+        }
+
+        return cookiesEnabled;
+    }
+
+
 
 if((window.location.pathname == "/") || (window.location.pathname == "/search")){
     $ondelete = $(".table tbody td a.delete");
     $ondelete.click(function(){
         var id = $(this).attr("data-id")
         var hasvoted=Cookies.get("Voted")
-        if(hasvoted !="true"){
-            var request = {
-                "url" : `https://coinbaba.herokuapp.com/api/users/${id}`,
-                "method" : "PUT"
-            }
+		if(cookiesEnabled()){
+			if(hasvoted !="true"){
+				var request = {
+					"url" : `https://coinbaba.herokuapp.com/api/users/${id}`,
+					"method" : "PUT"
+				}
 
-            
-                $.ajax(request).done(function(response){
-                    Cookies.set("Voted","true",{expires:1})
-                    location.reload();
-                })
-           }
-            else{
-                alert("Wait for 24 Hours to vote again");
-            }
+				
+					$.ajax(request).done(function(response){
+						Cookies.set("Voted","true",{expires:1})
+						location.reload();
+					})
+			   }
+				else{
+					alert("Wait for 24 Hours to vote again");
+				}
+				
+		}else
+		{
+			alert("Enable Cookies!");
+		}
         
 
     })
@@ -66,21 +86,27 @@ $("#coin_details").submit(function(event){
 
     var id = $(this).attr("data-id")
     var hasvoted=Cookies.get("Voted")
-    if(hasvoted !="true"){
-    var request = {
-        "url" : `https://coinbaba.herokuapp.com/api/users/${data.id}`,
-        "method" : "PUT",
+	 if(cookiesEnabled()){
+			if(hasvoted !="true"){
+			var request = {
+				"url" : `https://coinbaba.herokuapp.com/api/users/${data.id}`,
+				"method" : "PUT",
 
-    }
+			}
 
-    $.ajax(request).done(function(response){
-        Cookies.set("Voted","true",{expires:1})
-        location.reload()
-    })
-}
-else
-{
-    alert("Wait for 24 Hours to vote again");
-}
+			$.ajax(request).done(function(response){
+				Cookies.set("Voted","true",{expires:1})
+				location.reload()
+			})
+		}
+		else
+		{
+			alert("Wait for 24 Hours to vote again");
+		}
+	 }
+	 else
+	 {
+		 alert("Enable Cookies!");
+	 }
 
 })
