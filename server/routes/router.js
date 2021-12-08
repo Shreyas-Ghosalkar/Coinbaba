f53fd77ddf8f8422d27e801a06fa9c23dde12fb7
rohantirmale <34538719+rohantirmale@users.noreply.github.com>
2021-12-08 11:39:28 +0530

    Trying to get the working code

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const route = express.Router();
+const route = express.Router()
 
 const services = require('../services/render');
 const controller = require('../controller/controller');
@@ -14,15 +14,15 @@ route.get('/', services.homeRoutes);
  *  @description add users
  *  @method GET /add-user
  */
-route.get('/add-user', services.add_user);
+route.get('/add-user', services.add_user)
 
 /**
  *  @description for update user
  *  @method GET /update-user
  */
-route.get('/update-user', services.update_user);
-route.get('/coin_details', services.coin_details);
-route.get('/search', services.search);
+route.get('/update-user', services.update_user)
+route.get('/coin_details', services.coin_details)
+route.get('/search', services.search)
 
 // API
 route.post('/api/users', controller.create);
